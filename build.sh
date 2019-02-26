25d3e7b6e5a6cf291e8dbb4e13f9842def5a13c6
Pzqqt <821026875@qq.com>
2019-02-26 16:40:50 +0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export ARCH=arm64
+export SUBARCH=arm64
+export HEADER_ARCH=arm64
+export CLANG_PATH=/home/pzqqt/bin/android_prebuilts_clang_host_linux-x86_clang-r349610-8.1
+export CLANG_TRIPLE="aarch64-linux-gnu-"
+export CC="ccache $CLANG_PATH/bin/clang"
+export KBUILD_COMPILER_STRING="`cat $CLANG_PATH/VERSION`"
+export CROSS_COMPILE=/home/pzqqt/bin/gcc-linaro-6.5.0-2018.12-rc1-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=/home/pzqqt/bin/gcc-linaro-6.5.0-2018.12-rc1-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-
+
+export KBUILD_BUILD_HOST="lenovo"
+export KBUILD_BUILD_USER="pzqqt"
+
+export LOCALVERSION=-v4.9-rebase-uv
+make O=out mrproper && make O=out whyred_defconfig && make O=out -j6
+
