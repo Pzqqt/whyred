356e657215dc9998e494113bd4286b1c3f52224b
Pzqqt <821026875@qq.com>
2019-04-14 06:15:06 +0800

    build toolchain: back to linaro 7.4.1

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ export CLANG_PATH=/home/pzqqt/bin/android_prebuilts_clang_host_linux-x86_clang-r
 export CLANG_TRIPLE="aarch64-linux-gnu-"
 export CC="ccache $CLANG_PATH/bin/clang"
 export KBUILD_COMPILER_STRING="`cat $CLANG_PATH/VERSION`"
-export CROSS_COMPILE=/home/pzqqt/bin/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu/bin/aarch64-linux-gnu-
-export CROSS_COMPILE_ARM32=/home/pzqqt/bin/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabi/bin/arm-linux-gnueabi-
+export CROSS_COMPILE=/home/pzqqt/bin/gcc-linaro-7.4.1-2019.02-rc1-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=/home/pzqqt/bin/gcc-linaro-7.4.1-2019.02-rc1-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-
 
 export KBUILD_BUILD_HOST="lenovo"
 export KBUILD_BUILD_USER="pzqqt"
