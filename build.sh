68849a64c21aea4c2c222126c842a7a9f1789bd7
Pzqqt <821026875@qq.com>
2019-04-14 06:16:23 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -4,9 +4,7 @@ export ARCH=arm64
 export SUBARCH=arm64
 export HEADER_ARCH=arm64
 export CLANG_PATH=/home/pzqqt/bin/android_prebuilts_clang_host_linux-x86_clang-r353983
-export CLANG_TRIPLE="aarch64-linux-gnu-"
-export CC="ccache $CLANG_PATH/bin/clang"
-export KBUILD_COMPILER_STRING="`cat $CLANG_PATH/VERSION`"
+export KBUILD_COMPILER_STRING=$($CLANG_PATH/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 export CROSS_COMPILE=/home/pzqqt/bin/gcc-linaro-7.4.1-2019.02-rc1-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=/home/pzqqt/bin/gcc-linaro-7.4.1-2019.02-rc1-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-
 
@@ -14,5 +12,4 @@ export KBUILD_BUILD_HOST="lenovo"
 export KBUILD_BUILD_USER="pzqqt"
 
 export LOCALVERSION=-v5.2-rebase-uv
-make O=out mrproper && make O=out whyred_defconfig && make O=out -j6
-
+make O=out mrproper && make O=out whyred_defconfig && make O=out CLANG_TRIPLE=aarch64-linux-gnu- CC="ccache $CLANG_PATH/bin/clang" -j6
