9c87757f246fd2bcfd4b950674dab5cbbfe8c730
Pzqqt <821026875@qq.com>
2019-04-24 14:22:01 +0800

    Update build toolchain - clang 9.0.3

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 export ARCH=arm64
 export SUBARCH=arm64
 export HEADER_ARCH=arm64
-export CLANG_PATH=/home/pzqqt/bin/android_prebuilts_clang_host_linux-x86_clang-r353983
+export CLANG_PATH=/home/pzqqt/bin/android_prebuilts_clang_host_linux-x86_clang-5484270
 export KBUILD_COMPILER_STRING=$($CLANG_PATH/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 export CROSS_COMPILE=/home/pzqqt/bin/gcc-linaro-7.4.1-2019.02-rc1-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=/home/pzqqt/bin/gcc-linaro-7.4.1-2019.02-rc1-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-
